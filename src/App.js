87554203ca180e192a642a7bb6378b6411c3516f
r0c03xt <raymond.chan@walmartlabs.com>
2021-06-18 16:33:07 -0400

    add redux and users example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navigation from "./Navigation";
-import Home from "./Home";
-import Book from "./Book";
+import Navigation from "./components/navigation";
+import Home from "./components/home";
+import Users from "./components/users";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Navigation />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/book" exact component={Book} />
+          <Route path="/users" exact component={Users} />
         </Switch>
       </div>
     </Router>
--- src/components/Book.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Book() {
-  return <div>Book Page</div>;
-}
-
-export default Book;
